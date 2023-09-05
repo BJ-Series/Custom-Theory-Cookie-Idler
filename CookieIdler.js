a9ada7c173bbd80c656bb7d82bc7e26fdfe262e9
BJ-Series <143959532+BJ-Series@users.noreply.github.com>
2023-09-05 12:14:27 -0300

    Update CookieIdler.js

--- CookieIdler.js
+++ CookieIdler.js
@@ -22,7 +22,7 @@ import { QuaternaryEntry } from "../api/Theory";
 // Some parameters
 // If you're wondering why the refund button doesn't appear, please refer to line 646 and you would know something's up
 
-var id = "CookieIdlerC";
+var id = "eulers_formula";
 var name = "Cookie Idler";
 var description = "🍪👵🍪\nA game within a theory involving baking a copius amounts of cookies in exchange for something far greater...\n🍪👵🍪\n\n🍪==FEATURES==🍪\n🍪 Click, Bake, Farm, Produce your way into the big leagues. With 19 buildings to buy, empower, and upgrade.\n🍪 Experience a whole new level of text richness in theories like never before. Boatloads of text waiting to be read in all aspects, from the buildings, achievements, all the way to upgrades(nerdy mode included).\n🍪 Unique upgrades and intresting game mechanics will involve you to no end! Tasty Cookies, even tastier cookies, breaking the fourth wall, and changing the game itself.\n🍪Absolute lack of big and scary mathematics, rated E for Everyone\n\n🍪==CREDITS==🍪\n🍪 Orteil for bringing such a legendary game idea to life\n🍪 ellipsis for suggesting ideas for the UI\n🍪 skyhigh173 for reformatting the code so it looks better\nspqcey(randomname#9373) for proofreading and fixing a majority of the text\n🍪 Lava for fixing the legendary bug";
 var authors = "Sainen Lv.420 #2684";
